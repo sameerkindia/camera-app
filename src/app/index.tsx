8b7f5853538267bbfc8bb12b10795f1e060fd0e7
sameerkindia <sameerkhanaa5@gmail.com>
2025-10-08 00:07:29 +0530

    index file updated and app.tsx removed

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable, StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { Link } from "expo-router";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+// import * as FileSystem from "expo-file-system";
 
-const index = () => {
+const homeScreen = () => {
+
   return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
-}
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 24, fontWeight: "600" }}>home screen</Text>
 
-export default index
+      <Link href="/image-1">image 1</Link>
+      <Link href="/image-2">image 2</Link>
+      <Link href="/image-3">image 3</Link>
+
+      <Link href="/camera" asChild>
+        <Pressable style={style.floatingButton}>
+          <MaterialIcons name="photo-camera" size={30} color="white" />
+        </Pressable>
+      </Link>
+    </View>
+  );
+};
+
+export default homeScreen;
+
+const style = StyleSheet.create({
+  floatingButton: {
+    backgroundColor: "royalblue",
+    padding: 15,
+    borderRadius: 50,
+    position: "absolute",
+    bottom: 10,
+    right: 10,
+  },
+});
